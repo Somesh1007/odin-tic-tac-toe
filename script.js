868109b7bc73bbf9263af7ba6ec31556235313ca
Somesh Joshi <someshjoshi1007@gmail.com>
2023-09-07 03:06:55 +0530

    Add Initial HTML and CSS

---
+++ script.js
@@ -0,0 +1,26 @@
+const Player = (name, type) => {
+
+
+}
+
+
+const Gameboard = (() => {
+    let _gameboard = []
+
+    let playGame = (player1, player2) => {
+
+    }
+
+
+    return {
+        playGame
+    }
+}
+)()
+
+
+let player1 = Player('Somesh', 'Human')
+let player2 = Player('Testing', 'Human')
+
+let gameBoard = Gameboard(player1, player2)
+gameBoard.playGame()
